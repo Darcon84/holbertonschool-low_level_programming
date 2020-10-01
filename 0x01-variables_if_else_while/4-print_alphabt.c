908da5ac55407f024bc37214428dae9d037852e0
David <darcon84@gmail.com>
2020-10-01 10:53:30 -0500

    printabc

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,12 +7,12 @@
 
 int main(void)
 {
-  char abc = 'a';
-  while (abc <= 'z')
-    {
-      putchar(abc);
-      abc++;
-    }
-  putchar('\n');
-  return (0);
+char abc = 'a';
+while (abc <= 'z')
+{
+putchar(abc);
+abc++;
+}
+putchar('\n');
+return (0);
 }
