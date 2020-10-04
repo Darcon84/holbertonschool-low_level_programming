138ed2dd780b448369e43d1e34c271de4224d2ed
David <darcon84@gmail.com>
2020-10-04 16:41:21 -0500

    base16

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int x = 0;
+while (x < 10)
+{
+putchar(a + '0');
+putchar(',');
+putchar(' ');
+x++;
+}
+putchar('\n');
+return (0);
+}
